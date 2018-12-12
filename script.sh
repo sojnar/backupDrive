fe00236cf411903d351ff22957185316fa645b44
sojnar <ranjos@globomail.com>
2018-12-12 15:15:03 -0200

    #1 Criando configuracoes de email muttrc

--- script.sh
+++ script.sh
@@ -1,68 +1,75 @@
 #!/bin/bash
-#!/bin/bash
-# based on https://gist.github.com/deanet/3427090
-#
-# useful $HOME/.gdrive.conf options:
-#    curl_args="--limit-rate 500K --progress-bar"
 
+read -p "Digite o seu login do google sem o @gmail.com: " login
+stty -echo
+read -p "Difige sua senha do email google: " password
+stty echo
 
-browser="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36"
+configMutt(){
+  echo "
+    # Conta Gmail.
+    set from = \"$login@gmail.com\"
+    set realname = \"$login\"
+    set imap_user = \"$login@gmail.com\"
+    set imap_pass = \"$password\"
 
-destination_folder_id=${@: -1}
-if expr "$destination_folder_id" : '^[A-Za-z0-9]\{28\}$' > /dev/null
-then
-    # all but last word
-    set -- "${@:0:$#}"
-else
-    # upload to root
-    unset destination_folder_id
-fi
+    # Editor padrão.
+    set editor=nano
 
-if [ -e $HOME/.gdrive.conf ]
-then
-    . $HOME/.gdrive.conf
-fi
+    # Pastas IMAP.
+    set folder = \"imaps://imap.gmail.com:993\"
+    set spoolfile = \"+INBOX\"
+    set postponed = \"+[Gmail]/Drafts\"
 
-old_umask=`umask`
-umask 0077
+    # Pastas Locais.
+    set header_cache =~/.mutt/cache/headers
+    set message_cachedir =~/.mutt/cache/bodies
+    set certificate_file =~/.mutt/certificates
 
-if [ -z "$username" ]
-then
-    read -p "username: " username
-    unset token
-    echo "username=$username" >> $HOME/.gdrive.conf
-fi
+    # SMTP Config.
+    set smtp_url = \"smtp://$login@smtp.gmail.com:587/\"
+    set smtp_pass = \"$password\"
 
-if [ -z "$account_type" ]
-then
-    if expr "$username" : '^[^@]*$' > /dev/null || expr "$username" : '.*@gmail.com$' > /dev/null
-    then
-        account_type=GOOGLE
-    else
-        account_type=HOSTED
-    fi
-fi
+    # Special Keybindings.
+    bind editor <space> noop
+    macro index gi \"<change-folder>=INBOX<enter>\" \"Go to inbox\"
+    macro index ga \"<change-folder>=[Gmail]/All Mail<enter>\" \"Go to all mail\"
+    macro index gs \"<change-folder>=[Gmail]/Sent Mail<enter>\" \"Go to Sent Mail\"
+    macro index gd \"<change-folder>=[Gmail]/Drafts<enter>\" \"Go to drafts\"
 
-if [ -z "$password$token" ]
-then
-    read -s -p "password: " password
-    unset token
-    echo
-fi
+    # Mutt Session Security.
+    set move = no
+    set imap_keepalive = 900
 
-if [ -z "$token" ]
-then
-    token=`curl --silent --data-urlencode Email=$username --data-urlencode Passwd="$password" --data accountType=$account_type --data service=writely --data source=cURL "https://www.google.com/accounts/ClientLogin" | sed -ne s/Auth=//p`
-    sed -ie '/^token=/d' $HOME/.gdrive.conf
-    echo "token=$token" >> $HOME/.gdrive.conf
-fi
-umask $old_umask
+    # Cores.
+    color hdrdefault cyan default
+    color attachment yellow default
+    color header brightyellow default \"Subject: \"
+    color header brightyellow default \"Date: \"
+    color header brightyellow default \"From: \"
+    color quoted green default
+    color quoted1 cyan default
+    color quoted2 green default
+    color quoted3 cyan default
+    color error   red       default
+    color message  white      default
+    color indicator white      red
+    color status  white      blue
+    color tree   red       default
+    color search  white      blue
+    color markers  red       default
+    color index   yellow default '~O'
+    color index   yellow default '~N'
+    color index   brightred    default '~F'
+    color index   blue default  '~D'
 
-for file in "$@"
-do
-    slug=`basename "$file"`
-    mime_type=`file --brief --mime-type "$file"`
-    upload_link=`curl --silent --show-error --insecure --request POST --header "Content-Length: 0" --header "Authorization: GoogleLogin auth=${token}" --header "GData-Version: 3.0" --header "Content-Type: $mime_type" --header "Slug: $slug" "https://docs.google.com/feeds/upload/create-session/default/private/full${destination_folder_id+/folder:$destination_folder_id/contents}?convert=false" --dump-header - | sed -ne s/"Location: "//p`
-    echo "$file:"
-    curl --request POST --output /dev/null --data-binary "@$file" --header "Authorization: GoogleLogin auth=${token}" --header "GData-Version: 3.0" --header "Content-Type: $mime_type" --header "Slug: $slug" "$upload_link" $curl_args
-done
+    # Cores.
+
+    # emails.
+    color body  brightred black [\-\.+_a-zA-Z0-9]+@[\-\.a-zA-Z0-9]+
+
+    # URLs.
+    color body  brightblue black (https?|ftp)://[\-\.,/%~_:?&=\#a-zA-Z0-9]+
+  " > ~/.muttrc
+}
+configMutt
