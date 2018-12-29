87766db07bf391ab650cec229772bda7ac070212
sojnar <ranjos@globomail.com>
2018-12-29 19:12:54 -0200

    #13 Criação de dump Mysql/Postgres

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 log="/var/log/savedump.log"
+data=`date +%d%m%Y`
 
 installGdrive(){
   link64="https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download"
@@ -9,19 +9,19 @@ installGdrive(){
   if [ $arch == x86_64 ]
   then
     wget -b $link64 -O /bin/gdrive >> $log
-    echo "Instalando Gdrive!"
+    echo "Instalando Gdrive... Aguarde!"
     sleep 4
     chmod +x /bin/gdrive
   else
     wget -b $link32 -O /bin/gdrive >> $log
-    echo "Instalando Gdrive!"
+    echo "Instalando Gdrive... Aguarde!"
     sleep 4
     chmod +x /bin/gdrive
   fi
 }
 
 loadToken(){
-  sleep 3
+  sleep 5
   gdrive list >> $log | killall -s SIGINT gdrive
   linktoken=`tail -n5 $log | grep http`
 }
@@ -36,6 +36,7 @@ sendMail(){
     loadToken
     installMutt
     configMutt
+    confDB
   elif [ $resp == n ] || [ $resp == N ]
   then
     installGdrive
@@ -53,13 +54,14 @@ installMutt(){
   versionCentos=$(cat /proc/version | grep -i 'red hat' > /dev/null ; echo $?)
   if [ $versionUbuntu -eq 0 ]
   then
-    apt -f install -y >> $log 2>&1
-    apt install mutt -y >> $log 2>&1
     apt update -y >> $log 2>&1
+    apt -f install -y >> $log 2>&1
+    apt install mutt wget curl -y >> $log 2>&1
   elif [ $versionCentos -eq 0 ]
   then
-    yum install mutt -y >> $log 2>&1
-    yum update -y >> $log 2>&1
+    yum install mutt cyrus-sasl-plain wget -y >> $log 2>&1
+    yum install epel-release -y
+    yum install jq -y
   fi
 }
 
@@ -101,8 +103,80 @@ avaliableToken(){
 }
 
 tokenGdrive(){
+  sleep 4
   echo -n "Verifique seu email $login@gmail.com e insira o token para validação: "
   gdrive list >> $log
+}
+
+confDB(){
+  echo "Agora vamos configurar o backup do banco!"
+  read -p "Digite 'p' para postgres ou 'm' para mysql: " banco
+  case $banco in
+    p|P)
+
+        read -p "Deseja fazer um backup completo do banco (y|n) " dbase
+        if [ $dbase == y ] || [ $dbase == Y ]
+        then
+
+            if [ ! -d "/var/backupDrive" ]
+            then
+                mkdir /var/backupDrive
+            fi
+
+            echo "
+              pg_dumpall > /var/backupDrive/postgres_$data_all.dump
+            " > backupPostgres.sh
+
+        elif [ $dbase == n ] || [ $dbase == N ]
+        then
+
+            if [ ! -d "/var/backupDrive" ]
+            then
+                mkdir /var/backupDrive
+            fi
+
+            read -p "Digite o usuário do Banco: " userdb
+            stty -echo
+            read -p "Digite a senha do $userdb: " passdb
+            stty echo
+
+            echo "#!/bin/bash
+              pg_dump -U $userdb -d $dbase > /var/backupDrive/postgres_$data_$dbase.dump
+            " > backupPostgres.sh
+
+        fi
+    ;;
+    m|M)
+
+        read -p "Deseja fazer um backup completo do banco (y|n) " dbase
+        if [ $dbase == y || $dbase == Y ]
+        then
+
+             if [ ! -d "/var/backupDrive" ]
+             then
+                  mkdir /var/backupDrive
+              fi
+
+              echo "#!/bin/bash
+                mysqldump --all-databases > /var/backupDrive/mysql_$data_all.dump
+              " > backupMysql.sh
+
+        elif [ $dbase == n || $dbase == N ]
+        then
+
+            read -p "Digite o usuário do Banco: " userdb
+            stty -echo
+            read -p "Digite a senha do $userdb: " passdb
+            stty echo
+
+            echo "#!/bin/bash
+              mysqldump -u $userdb -p $passdb $dbase > mysql_$data_$dbase.dump
+             " > backupMysql.sh
+
+        fi
+    ;;
+    *)
+  esac
 }
 
 #startBackupMysql(){
